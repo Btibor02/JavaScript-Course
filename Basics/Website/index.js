deaf0fd1969e6f0e7df292c38ebb774e3b2511a5
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-01 18:58:07 +0100

    Episode 3

--- Basics/Website/index.js
+++ Basics/Website/index.js
@@ -23,9 +23,32 @@ let online = true;
 let forSale = false;
 
 let fullName = "Elek Teszt";
-let age = 25;
 let isStudent = true;
 
 document.getElementById("p1").textContent = fullName;
 document.getElementById("p2").textContent = `Your age is ${age}.`;
 document.getElementById("p3").textContent = isStudent; */
+
+// ! Arithmetic operators - Episode 3
+
+/*let students = 30;
+students += 2;
+students -= 2;
+students *= 2;
+students /= 2;
+
+students **= 2; // exponent
+let extraStudents = students % 2; // modulo
+
+students++;
+students--;
+
+
+    //operator precedence
+    //1. parenthesis ()
+    //2. exponents
+    //3. multiplication & division & modulo
+    //4. addition & subtraction
+
+
+let result = 1 + 2 * 3 + 4 ** 2; */

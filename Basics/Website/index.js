17832a4fdea9deaf3c0e49840c5eddb679e2f952
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-01 18:47:47 +0100

    Episode 1 & 2

---
+++ Basics/Website/index.js
@@ -0,0 +1,31 @@
+// ! Basics - Episode 1
+
+/*console.log(`Hello`);
+
+window.alert(`This is an alert!`)
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!`; */
+
+// ! Variables - Episode 2
+
+/*let x;
+x = 100
+console.log(`Hello ${x}!`);
+
+let age = 25;
+let price = 10.99;
+console.log(typeof age);
+
+let firstName = "Elek";
+
+let online = true;
+let forSale = false;
+
+let fullName = "Elek Teszt";
+let age = 25;
+let isStudent = true;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = `Your age is ${age}.`;
+document.getElementById("p3").textContent = isStudent; */

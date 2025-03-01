8a75bb1084b0bf410d2dc6d50477155943113d54
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-01 19:47:46 +0100

    Episode 8

--- Basics/Beginning/index.js
+++ Basics/Beginning/index.js
@@ -109,3 +109,33 @@ document.getElementById("mySubmitRadius").onclick = function(){
 }*/
 
 
+// ! Math object - Episode 8
+
+/*console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3.21;
+let x2 = 3.99;
+let y = 2;
+let z;
+
+z = Math.round(x);
+z = Math.floor(x2); // always round down
+z = Math.ceil(x); // always round up
+z = Math.trunc(x); // get rid of numbers after ,
+
+z = Math.pow(x, y);
+z = Math.sqrt(x);
+z = Math.log(x);
+
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+
+z = Math.abs(x); // absolute value
+z = Math.sign(x);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);*/
+
+

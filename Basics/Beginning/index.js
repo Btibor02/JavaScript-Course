24322f0d0e06736b38f043e1f56a507240cf9d42
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-02 16:40:08 +0100

    Episode 11

--- Basics/Beginning/index.js
+++ Basics/Beginning/index.js
@@ -173,4 +173,30 @@ if(age1 >= 16) {
     console.log("You must be at least 16 to have a licence");
 }*/
 
+// ! Checked property - Episode 11
 
+/*const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmitCard = document.getElementById("mySubmitCard");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmitCard.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed!`;
+    } else {
+        subResult.textContent = `You are not subscribed!`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`;
+    } else if(masterCardBtn.checked) {
+        paymentResult.textContent = `You are paying with MasterCard`;
+    } else if(payPalBtn.checked) {
+        paymentResult.textContent = `You are paying with PayPal`;
+    } else {
+        paymentResult.textContent = `You must select a payment method!`;
+    }
+}*/

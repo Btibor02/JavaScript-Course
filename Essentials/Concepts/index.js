b83d9bea5f9a8aa6707c616bc72f5e8d21f32fa9
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-02 17:44:29 +0100

    Episode 22

---
+++ Essentials/Concepts/index.js
@@ -0,0 +1,25 @@
+// ! Functions - Episode 22
+
+/*function happyBirthday(){
+    console.log("Happy Birthday to you!");
+}
+
+happyBirthday();
+
+function nameHappyBirthday(username, age){
+    console.log(`Happy Birthday to ${username}!`);
+    console.log(`Happy ${age}. Birthday to ${username}!`);
+}
+
+nameHappyBirthday("Elek", 20);
+
+function add(x, y){
+    let result = x + y;
+    return result;
+
+    // or return x + y;
+}
+
+let answer = add(5, 15);*/
+
+

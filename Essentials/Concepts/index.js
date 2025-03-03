1d9561f2d0f9dcf1c03ef61f569796eef4e4deff
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-03 15:35:30 +0100

    Episode 33

--- Essentials/Concepts/index.js
+++ Essentials/Concepts/index.js
@@ -134,3 +134,17 @@ function square(element) {
     return Math.pow(element, 2);
 }*/
 
+// ! .filter() - Episode 33
+
+// ? .filter() = creates a new array by filtering out elements
+
+/*let numbers = [1, 2, 3, 4, 5];
+
+let evenNums = numbers.filter(isEven);
+
+console.log(evenNums);
+
+function isEven(element) {
+    return element % 2 === 0;
+}*/
+

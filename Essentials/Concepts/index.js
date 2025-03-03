605cd1b850737686179020b3ec9e08b5dcb026ac
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-03 15:32:51 +0100

    Episode 32

--- Essentials/Concepts/index.js
+++ Essentials/Concepts/index.js
@@ -120,3 +120,17 @@ function display(element) {
     console.log(element);
 }*/
 
+// ! .map() - Episode 32
+
+// ? .map() = accepts a callback and applies that function
+// ?            to each element of an array, then return a new array
+// ?            Similar to .forEach()
+
+/*const numbers = [1, 2, 3, 4, 5];
+
+const squares = numbers.map(square);
+
+function square(element) {
+    return Math.pow(element, 2);
+}*/
+

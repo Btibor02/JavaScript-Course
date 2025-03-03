963307ca46574f5c025fc528fed0318aadd7b331
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-03 15:46:53 +0100

    Episode 35

--- Essentials/Concepts/index.js
+++ Essentials/Concepts/index.js
@@ -166,3 +166,29 @@ function sum(accumulator, element) {
     return accumulator + element;
 }*/
 
+// ! Function expressions - Episode 35
+
+// ? function declaration 
+
+/*function hello(){
+    console.log("Hello");
+}
+
+// ? function expression
+
+const hello = function(){
+    console.log("Hello");
+}
+
+hello();
+
+setTimeout(function(){
+    console.log("Hello");
+}, 3000);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map(function(element){
+    return Math.pow(element, 2);
+});*/
+
+

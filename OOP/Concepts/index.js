5a328f13d6eb22b0290a695a915f2c0f5980bba8
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-03 16:08:47 +0100

    Episode 37

---
+++ OOP/Concepts/index.js
@@ -0,0 +1,14 @@
+// ! JavaScript Objects - Episode 37
+
+/*const person = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 24,
+    isEmployed: true,
+    sayHello: function(){
+        console.log("Hi!")
+    },
+}
+
+console.log(person1.firstName);
+person.sayHello();*/

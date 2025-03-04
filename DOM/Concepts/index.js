c3c2972103c89af11a1c53ba50f34a69d63fe29b
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-03-04 16:10:36 +0100

    Episode 59

---
+++ DOM/Concepts/index.js
@@ -0,0 +1,10 @@
+// ! Document Object Model - Episode 59
+
+/*console.dir(document);
+document.title = "My website";
+document.body.style.backgroundColor = "hsl(0, 6.90%, 74.30%)";
+
+const username = "Teszt";
+const welcomeMsg = document.getElementById("welcome-msg");
+welcomeMsg.textContent += username === "" ? `Guest` : username;*/
+

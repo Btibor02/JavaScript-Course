df4a7052f022f1a1ca22e6b8f96de2399d8490be
Tibor Blascsok <tiborblascsok02@gmail.com>
2025-04-02 19:26:23 +0200

    Episode 70

---
+++ Async/index.js
@@ -0,0 +1,6 @@
+// ! Callback hell - Episode 70
+
+// Callback Hell = Situation in JS where callbacks are nested within other callbacks
+//                 to the degree where the code is difficult to read.
+//                 Use Promises + async/await to avoid it.
+
